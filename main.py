c9af88af21ac26adb65d088a3f11210b890cc036
Isha vijay` <ishavijay@Ishas-MacBook-Air.local>
2025-07-30 18:38:40 +0530

    Update MongoDB connection string for Render SSL handshake fix

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ import smtplib
 import ssl
 
 # MongoDB Atlas connection
-MONGO_URI = "mongodb+srv://harshitvj24:Harshit%40321@cluster0.2rw2irv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_URI = "mongodb+srv://harshitvj24:Harshit%40321@cluster0.2rw2irv.mongodb.net/?retryWrites=false&w=majority&tls=true&tlsAllowInvalidCertificates=true&appName=Cluster0"
 try:
     mongo_client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000, tls=True, tlsAllowInvalidCertificates=True)
     mongo_client.server_info()  # Force connection on a request as a test
